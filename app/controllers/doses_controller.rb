846e4d09b1e41fe7dabfec34780a9d34d10d963a
beatriz andre <biacaram@hotmail.com>
2020-11-19 18:24:18 -0200

    rake green

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,16 @@
 class DosesController < ApplicationController
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to @dose
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
